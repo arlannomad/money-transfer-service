41c6cdcb4835accc36c94bbe7a56fad3045a4dcd
arlan <arlannomad@mail.com>
2023-07-30 10:58:44 +0600

    Commented 3 classes

--- src/test/java/kz/almaty/moneytransferservice/MoneyTransferServiceApplicationTests.java
+++ src/test/java/kz/almaty/moneytransferservice/MoneyTransferServiceApplicationTests.java
@@ -1,16 +1,16 @@
-package kz.almaty.moneytransferservice;
-
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-
-@SpringBootTest
-class MoneyTransferServiceApplicationTests {
-
-    @Disabled
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package kz.almaty.moneytransferservice;
+//
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//
+//@SpringBootTest
+//class MoneyTransferServiceApplicationTests {
+//
+//    @Disabled
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
